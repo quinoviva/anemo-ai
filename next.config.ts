151c906133619f963c046f1ae661b1e775dcd0e4
Cyril Quinoviva <cyquinoviva@gmail.com>
2025-11-08 02:15:04 +0000

    Disable Dev Tools for this project To disable this UI completely, set de

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -23,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
